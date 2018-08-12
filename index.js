6e10c924f16da2acc5a1ab1e25584f82ecdda4d8
sykes836 <bernard.sykes836@gmail.com>
2018-08-12 13:45:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,16 @@ You should NOT try to redefine any of the above variables.
 */
 
 // Write your code below this line!
+let newID = 1000000000
+
+beforeEach(function() {
+  newID = (1000000000 + oldID)
+ });
+ 
+ function ageIsValid (x, y) {
+  if (Number.isInteger(y / x)) {
+    return 'True';
+  }
+  else
+  return 'False';
+};
